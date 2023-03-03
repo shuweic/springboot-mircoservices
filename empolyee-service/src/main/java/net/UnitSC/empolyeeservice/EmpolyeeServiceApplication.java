31338962caea466dd636c35d8521305f2f83e695
cuicoms <cuicoms@iastate.edu>
2023-03-03 13:51:02 -0600

    setup Microservices communication using WebClient

--- empolyee-service/src/main/java/net/UnitSC/empolyeeservice/EmpolyeeServiceApplication.java
+++ empolyee-service/src/main/java/net/UnitSC/empolyeeservice/EmpolyeeServiceApplication.java
@@ -4,14 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmpolyeeServiceApplication {
 
 	// define spring bean
+//	@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
+
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public WebClient webClient() {
+		return WebClient.builder().build();
 	}
 
 	public static void main(String[] args) {
--- empolyee-service/src/main/java/net/UnitSC/empolyeeservice/service/Impl/EmployeeServiceImpl.java
+++ empolyee-service/src/main/java/net/UnitSC/empolyeeservice/service/Impl/EmployeeServiceImpl.java
@@ -11,6 +11,7 @@ import net.UnitSC.empolyeeservice.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -18,7 +19,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+    private WebClient webClient;
+//    private RestTemplate restTemplate;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
 
@@ -37,12 +39,17 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         Employee employee = employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
-
-        DepartmentDto departmentDto = responseEntity.getBody();
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
 //        EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
                 employee.getFirstName(),
